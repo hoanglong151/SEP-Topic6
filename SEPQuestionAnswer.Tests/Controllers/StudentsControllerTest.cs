dc51f8a6d920aa0f83c6de6b4bea6fcafeeec9f9
Pham MinhLong <phamminhlong10@outlook.com>
2021-06-11 14:31:45 +0700

    student list & unit test

---
+++ SEPQuestionAnswer.Tests/Controllers/StudentsControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SEPQuestionAnswer.Areas.Admin.Controllers;
+using SEPQuestionAnswer.Models;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using PagedList;
+
+namespace SEPQuestionAnswer.Tests.Controllers
+{
+    [TestClass]
+    public class StudentsControllerTest
+    {
+        StudentsController controller = new StudentsController();
+        SEP24Team10Entities db = new SEP24Team10Entities();
+        [TestMethod]
+        public void TestIndex()
+        {           
+            var list = controller.Index(1) as ViewResult;
+            Assert.IsNotNull(list);
+            Assert.IsNotNull(list.Model as PagedList<Student>);           
+        }
+    }
+}
---
+++ SEPQuestionAnswer/Areas/Admin/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using SEPQuestionAnswer.Models;
+namespace SEPQuestionAnswer.Areas.Admin.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private SEP24Team10Entities db = new SEP24Team10Entities();
+        // GET: Admin/Students
+        public ActionResult Index(int? page)
+        {
+            int sizePage = 10;
+            int pageNumber = (page ?? 1);
+            return View(db.Students.ToList().ToPagedList(pageNumber, sizePage));
+        }
+    }
+}
