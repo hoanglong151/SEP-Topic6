dd5786a6af5bb55fc96cf537448c330da1f9becf
long nguyen <nguyenphamhoanglong2000@gmail.com>
2021-07-28 11:00:15 +0700

    Upload Image Question & Link

--- SEPQuestionAnswer/Areas/Admin/Controllers/QuestionsController.cs
+++ SEPQuestionAnswer/Areas/Admin/Controllers/QuestionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
@@ -219,6 +220,45 @@ namespace SEPQuestionAnswer.Areas.Admin.Controllers
                 ModelState.AddModelError("Answer", "Câu trả lời không được để trống hoặc nhập ký tự khoảng trắng");
             }
         }
+
+        [HttpPost]
+        public ActionResult TinyMceUpload(HttpPostedFileBase file)
+        {
+            //Response.AppendHeader("Access-Control-Allow-Origin", "*");
+
+            //var location = SaveFile(Server.MapPath("~/uploads/"), file);
+            var location = "http://cntttest.vanlanguni.edu.vn:18080/SEP24Team10" + SaveFile(Server.MapPath(@"~/uploads/"), file);
+            return Json(new { location }, JsonRequestBehavior.AllowGet);
+        }
+
+        private static string SaveFile(string targetFolder, HttpPostedFileBase file)
+        {
+            const int megabyte = 1024 * 1024;
+
+            if (!file.ContentType.StartsWith("image/"))
+            {
+                throw new InvalidOperationException("Invalid MIME content type.");
+            }
+
+            var extension = Path.GetExtension(file.FileName.ToLowerInvariant());
+            string[] extensions = { ".gif", ".jpg", ".png", ".svg", ".webp", ".jpeg" };
+            if (!extensions.Contains(extension))
+            {
+                throw new InvalidOperationException("Invalid file extension.");
+            }
+
+            if (file.ContentLength > (8 * megabyte))
+            {
+                throw new InvalidOperationException("File size limit exceeded.");
+            }
+
+            var fileName = Guid.NewGuid() + extension;
+            var path = Path.Combine(targetFolder, fileName);
+            file.SaveAs(path);
+
+            return Path.Combine("/uploads", fileName).Replace('\\', '/');
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
